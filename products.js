3e7a80068a882dbf9db7f5320980d990482ee057
irondometrader <irondometrader@gmail.com>
2026-03-08 17:01:57 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,35 @@
+const products = [
+
+{
+name:"Iron Dome Trader EA",
+image:"https://picsum.photos/500/300",
+link:"product/iron-dome-ea.html"
+}
+
+]
+
+const container = document.getElementById("products")
+
+products.forEach(p=>{
+
+const card = document.createElement("div")
+
+card.className="card"
+
+card.innerHTML=`
+
+<img src="${p.image}">
+
+<h3>${p.name}</h3>
+
+<a href="${p.link}">
+
+<button>View Details</button>
+
+</a>
+
+`
+
+container.appendChild(card)
+
+})
